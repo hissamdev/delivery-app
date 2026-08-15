a2f01be1d2b1f49b7e4a41587d10703924486a1e
Hissam <hissamdev@gmail.com>
2026-08-15 10:39:16 +0600

    About to try custom return

--- server-laravel/app/Http/Controllers/ProductController.php
+++ server-laravel/app/Http/Controllers/ProductController.php
@@ -7,11 +7,15 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    // get all
     public function index() {
         return Product::all();
     }
+
+    // get one
     public function show(Product $product) {
         return $product;
     }
+
+
 }
