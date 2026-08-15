670d40a56daaf11f5f6a47b076ba3c32d52cfd7b
Hissam <hissamdev@gmail.com>
2026-08-15 16:29:19 +0600

    Controller returns a custom object

--- server-laravel/app/Http/Controllers/ProductController.php
+++ server-laravel/app/Http/Controllers/ProductController.php
@@ -8,14 +8,26 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     // get all
-    public function index() {
-        return Product::all();
+    public function index()
+    {
+
+        return [
+            'success' => true,
+            'message' => "Products fetched successfully",
+            'products' => Product::all()
+        ];
+
+        // Example
+        // return response->json([
+        //     'success' => true,
+        //     'message' => "Products fetched successfully",
+        //     'products' => Product::all()
+        // ], 200);
     }
 
     // get one
-    public function show(Product $product) {
+    public function show(Product $product)
+    {
         return $product;
     }
-
-
 }
