05a89201275204b2bfddcf9271de156a5ebf4871
Hissam <hissamdev@gmail.com>
2026-09-09 06:24:45 +0600

    Created basic header with admin button

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../components/header";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className=""> 
-            <button>Admin</button>
-        </div>
+        <header className="px-8 py-4 flex"> 
+            <Link href="/dashboard" className="ml-auto px-3 py-1 bg-blue-400 hover:bg-blue-400/90 rounded-full text-sm text-white cursor-pointer">Admin</Link>
+        </header>
     )
 }
