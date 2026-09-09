96aa73e9485f35d1c3ab662875e946a5cb5c007f
Hissam <hissamdev@gmail.com>
2026-09-09 06:51:34 +0600

    wip: Dashboard sidebar

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 export default function Dashboard() {
     return (
         <main>
-            <aside className="">
+            
+            <aside className="fixed top-0 left-0 border w-70 h-screen">
                 <div>Dashboard</div>
             </aside>
         </main>
