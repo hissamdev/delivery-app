1c450c120cbdbbe219bd73ac23b4ee9d1d18fac2
Hissam <hissamdev@gmail.com>
2026-09-25 13:38:09 +0600

    Initialized Next.js UI

--- server-laravel/routes/api.php
+++ server-laravel/routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource("products", ProductController::class);
 
-// Route::resource("products", ProductController::class, "index");
+// Route::get("products", ProductController::class, "index");
 
 Route::get('/user', function (Request $request) {
     return $request->user();
