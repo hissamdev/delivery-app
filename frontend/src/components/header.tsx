12fe3347cd135afa543ea4f68f0cdd27fa7970f2
Hissam <hissamdev@gmail.com>
2026-09-25 13:41:12 +0600

    Removed older test UI

--- frontend/src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Header() {
-    return (
-        <header className="px-8 py-4 flex"> 
-            <Link href="/dashboard" className="ml-auto px-3 py-1 bg-blue-400 hover:bg-blue-400/90 rounded-full text-sm text-white cursor-pointer">Admin</Link>
-        </header>
-    )
-}
